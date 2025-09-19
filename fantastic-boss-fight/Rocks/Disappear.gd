1a462db770a8f4f586d5cf8b92b35b2d3b85df4d
fantasticyeyplustiger <143634964+fantasticyeyplustiger@users.noreply.github.com>
2025-09-18 22:33:14 -0400

    optimal rock spawning algorithm

--- fantastic-boss-fight/Rocks/Disappear.gd
+++ fantastic-boss-fight/Rocks/Disappear.gd
@@ -1,7 +1,32 @@
 extends Node3D
 
 func _ready() -> void:
-	await get_tree().create_timer(2.0).timeout
+	
+	for child in self.get_children():
+		
+		if not child is MeshInstance3D:
+			continue
+		
+		var new_position : Vector3
+		new_position.x = randf_range(-1.5, 1.5)
+		new_position.y += randf_range(-0.1, 0.1)
+		new_position.z = randf_range(-0.5, 0.5)
+		
+		child.position = new_position
+		
+		var new_rotation : Vector3
+		new_rotation.x = randf_range(0.0, PI * 2)
+		new_rotation.y = randf_range(0.0, PI * 2)
+		new_rotation.z = randf_range(0.0, PI * 2)
+		
+		var new_scale : float = randf_range(1.2, 2.0)
+		child.scale *= Vector3(new_scale, new_scale, new_scale)
+		
+		child.rotation = new_rotation
+	
+	var disappearing_time : float = randf_range(1.0, 2.5)
+	
+	await get_tree().create_timer(disappearing_time).timeout
 	
 	for child in self.get_children():
 		
@@ -14,9 +39,9 @@ func _ready() -> void:
 			child,
 			"transparency",
 			1.0,
-			3.0
+			disappearing_time / 2.0
 		)
 	
-	await get_tree().create_timer(4.5).timeout
+	await get_tree().create_timer(5.0).timeout
 	
 	queue_free()
